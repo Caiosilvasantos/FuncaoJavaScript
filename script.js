1a722936a08b8a7d3318df1734a2ef122ce4c3f0
Perkles <caiosilvasantos15@gmail.com>
2022-11-20 11:56:38 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//document.write("<h2>" + "Linha 2" + "</h2>");
+
+//document.write("Linha 2") ;
+
+function hello(){
+    var nome;
+    nome = prompt("Qual é o seu nome?")
+    alert("Hello " + nome)
+}
